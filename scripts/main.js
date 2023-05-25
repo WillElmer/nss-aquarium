0e63e699b61fc73526348e41a1380ce7af42975e
Will Elmer <willpelmer12@gmail.com>
2023-05-25 13:31:03 -0500

    My first version of my aquarium code

---
+++ scripts/main.js
@@ -0,0 +1,44 @@
+const database = {
+    fish: [{
+        type: "Smallmouth Bass",
+        name: "Cat, cape wild",
+        size: 3.7
+      }, {
+        type: "Largemouth Bass",
+        name: "Spider, wolf",
+        size: 9.5
+      }, {
+        type: "Largemouth Bass",
+        name: "Asian lion",
+        size: 9.9
+      }, {
+        type: "GoldFish",
+        name: "Beaver, european",
+        size: 1.2
+      }, {
+        type: "Smallmouth Bass",
+        name: "Tropical buckeye butterfly",
+        size: 3.4
+      }, {
+        type: "GoldFish",
+        name: "Long-tailed spotted cat",
+        size: 0.3
+      }, {
+        type: "Largemouth Bass",
+        name: "Bison, american",
+        size: 8.5
+      }, {
+        type: "Largemouth Bass",
+        name: "Anaconda (unidentified)",
+        size: 7.6
+      }, {
+        type: "Smallmouth Bass",
+        name: "Brown lemur",
+        size: 3.8
+      }, {
+        type: "Largemouth Bass",
+        name: "Slender-billed cockatoo",
+        size: 9.4
+  }
+ ]
+}
